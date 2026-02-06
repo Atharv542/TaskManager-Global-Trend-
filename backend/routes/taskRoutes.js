6e875dd079072b352cf17e29645d9316631e259d
Atharv542 <atharvragdwal05@gmail.com>
2026-02-06 16:29:04 +0530

    Added changes

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Task from "../models/Task.js";
-import auth from "../middleware/auth.js";
+import auth from "../middlewares/auth.js";
 
 const router = express.Router();
 router.use(auth);
