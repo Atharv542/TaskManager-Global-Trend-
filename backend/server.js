43faa21c4eeb7aa10c5451ef7686cb2ff40ae9e7
Atharv542 <atharvragdwal05@gmail.com>
2026-02-06 16:00:07 +0530

    Added changes

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,12 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://task-manager-global-trend.vercel.app/"
-  ],
+  origin:"https://task-manager-global-trend.vercel.app",
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type","Authorization"],
-  credentials: true
 }));  
+
+app.options("*", cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
